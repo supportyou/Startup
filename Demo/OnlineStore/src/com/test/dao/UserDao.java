ee89e8a2a0acf0bcc986049844af004273bf4a66
Shixin <supportyou@126.com>
2016-04-20 11:45:11 +0800

    System

--- Demo/OnlineStore/src/com/test/dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package com.test.dao;
-
-import com.test.domain.User;
-
-public interface UserDao {
-
-	/// 登录验证，如果失败返回null
-	public User loginCheck(String name, String password);
-}
--- Demo/OnlineStore/src/com/test/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.test.service;
-
-import com.test.domain.User;
-
-public interface UserService {
-
-	//用户登录方法
-	public User login(String name,String password);
-}
